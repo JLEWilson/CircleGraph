cac6542bbf12474886b7fdacd41dd78d93c211b3
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-12-02 16:49:10 -0900

    add commented out test code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import GenerateForm from "./components/generate-form";
 import Graph from "./components/graph";
+import "./styles/Graph.css";
 
 function App() {
   const [slices, setSlices] = React.useState(0);
@@ -11,10 +12,43 @@ function App() {
     setSlices(s);
     setRows(r);
     setIsGraphGenerated(true);
-    console.log(`Slices: ${slices} Rows: ${rows} `);
+    console.log(`Slices: ${s} Rows: ${r} `);
   };
   return (
     <div>
+      {/* <svg height="0" width="0">
+        <defs>
+          <clipPath clipPathUnits="objectBoundingBox" id="sector">
+            <path
+              fill="none"
+              stroke="#111"
+              stroke-width="1"
+              class="sector"
+              d="M0.5,0.5 l0.5,0 A0.5,0.5 0 0,0 0.75,.066987298 z"
+            ></path>
+          </clipPath>
+        </defs>
+      </svg>
+      <ul class="menu">
+        <li class="one"></li>
+        <li class="two"></li>
+        <li class="three"></li>
+        <li class="four"></li>
+        <li class="five"></li>
+        <li class="six"></li>
+      </ul> */}
+      {/* <div className="PizzaContainer">
+        <div className="PizzaBackground"></div>
+        <div className="PizzaSlice hold" style={{}}>
+          <div
+            className="Pizza"
+            style={{
+              backgroundColor: "Red",
+              transform: "rotate(60deg)",
+            }}
+          ></div>
+        </div>
+      </div> */}
       {isGraphGenerated ? (
         <Graph slices={slices} rows={rows} />
       ) : (
