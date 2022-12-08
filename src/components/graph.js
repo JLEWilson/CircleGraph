421632fb94c16e1990c43a8df71c260fb3a3a267
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-12-07 15:44:37 -0900

    adjust size to be based on window

--- src/components/graph.js
+++ src/components/graph.js
@@ -2,6 +2,11 @@ import React from "react";
 import "../styles/Graph.css";
 import CenterSlice from "./slice";
 function Graph(props) {
+  const smallestSize =
+    window.innerWidth - 220 < window.innerHeight
+      ? window.innerWidth - 220
+      : window.innerHeight;
+  const allowance = Math.floor(smallestSize / props.rows);
   const centerSlicesArr = [];
   let count = 0;
   for (let i = 1; i <= props.rows; i++) {
@@ -9,10 +14,12 @@ function Graph(props) {
     for (let j = 0; j < x; j++) {
       centerSlicesArr.push(
         <CenterSlice
+          color={props.color}
           index={count}
           key={`Row${i}:Slice${j}`}
           slices={props.slices}
           row={i}
+          allowance={allowance}
         />
       );
       count--;
